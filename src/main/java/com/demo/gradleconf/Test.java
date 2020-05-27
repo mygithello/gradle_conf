326340fa5b1ebf81295857f4269be04694990fc2
jiayachong <1357602588@qq.com>
2020-05-27 22:33:52 +0800

    conf makejar task to  jar file and test

---
+++ src/main/java/com/demo/gradleconf/Test.java
@@ -0,0 +1,15 @@
+package com.demo.gradleconf;
+
+/**
+ * @author jyc
+ * @title: Test
+ * @projectName gradle_conf
+ * @description:
+ * @date 20-5-27 下午9:42
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("----------------------------tst --");
+    }
+}
